94f0a96cd825a87f99b0008ba9b03da8ceed528f
Harshitha1927 <puthalapattuharshitha@gmail.com>
2026-08-29 10:09:48 +0530

    Create code.py

---
+++ Lab Manual/CO-2/Experiment-9/code.py
@@ -0,0 +1,11 @@
+student(ravi, cse101).
+student(anu, ece102).
+student(kiran, ece103).
+
+teacher(kumar, cse101).
+teacher(priya, ece102).
+teacher(raj, ece103).
+
+subject(cse101, programming).
+subject(ece102, electronics).
+subject(ece103, signals).
