55682f4d90d8fa4521de4f1b323f99e8fbe4a09c
Harshitha1927 <puthalapattuharshitha@gmail.com>
2026-08-29 10:04:38 +0530

    Implement minimax algorithm in code.py

---
+++ Lab Manual/CO-2/Experiment-3/code.py
@@ -0,0 +1,12 @@
+def minimax(depth, isMax):
+    if depth == 0:
+        return 0
+
+    if isMax:
+        return max(minimax(depth-1, False)+1,
+                   minimax(depth-1, False)-1)
+    else:
+        return min(minimax(depth-1, True)+1,
+                   minimax(depth-1, True)-1)
+
+print("Best Score:", minimax(3, True))
